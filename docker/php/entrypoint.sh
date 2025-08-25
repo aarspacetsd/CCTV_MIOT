c729cb5b8c04bebdf1f4bfd9c5cdb8b3d89033ca
aarspacetsd <emailkamu@example.com>
2025-08-26 03:25:28 +0700

    struktur baru

--- docker/php/entrypoint.sh
+++ docker/php/entrypoint.sh
@@ -5,23 +5,24 @@ set -e
 cd /var/www/html
 
 # Hanya lakukan hal-hal penting untuk startup
-# Jika file .env tidak ada, buat dari .env.example dan generate key
 if [ ! -f ".env" ]; then
     echo "Creating .env file..."
     cp .env.example .env
 
-    # PERBAIKAN: Pastikan baris APP_KEY ada sebelum men-generate
     echo "Ensuring APP_KEY is set..."
     grep -q "APP_KEY=" .env || echo "APP_KEY=" >> .env
     php artisan key:generate
 fi
 
-# PERBAIKAN: Tambahkan 'chown' untuk memastikan www-data memiliki kepemilikan folder
+# PERBAIKAN: Buat symbolic link untuk storage
+echo "Creating storage link..."
+php artisan storage:link
+
+# Perbaiki izin folder
 echo "Fixing ownership and permissions for storage and cache..."
 chown -R www-data:www-data storage bootstrap/cache
 chmod -R 775 storage bootstrap/cache
 
 # Jalankan perintah utama dari Dockerfile (yaitu "php-fpm")
-# Skrip 'docker-php-entrypoint' akan memulai php-fpm sebagai www-data
 echo "Handing over to main container command (php-fpm)..."
 exec docker-php-entrypoint "$@"
