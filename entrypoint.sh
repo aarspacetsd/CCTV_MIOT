8b74e67ff1c1c8c6fb387d55bd4909e5328d4586
aarspacetsd <emailkamu@example.com>
2025-08-25 18:23:28 +0700

    Fix: Update Dockerfile dependencies

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,16 +1,18 @@
 #!/bin/sh
 set -e
 
-# Salin file environment jika .env belum ada
+# --- PERUBAHAN DI SINI ---
+# Salin dari .env.example (yang ada di repo Anda) jika .env belum ada
 if [ ! -f ".env" ]; then
-    cp .env.docker .env
+    echo "Creating .env file from .env.example"
+    cp .env.example .env
 fi
 
 # Instal dependensi Composer
 # Opsi --no-scripts mencegah error jika APP_KEY belum ada
 composer install --no-interaction --prefer-dist --optimize-autoloader --no-scripts
 
-# Buat kunci aplikasi (APP_KEY) jika belum ada
+# Buat kunci aplikasi (APP_KEY)
 php artisan key:generate
 
 # Jalankan migrasi database
